bdd559f94c1aa01edde01006a38e63060544099f
dok7-software <dok7io.master@gmail.com>
2026-07-09 12:26:57 +0200

    DA: path gamificacion

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
+  basePath: '/gamificacion',
   outputFileTracingRoot: path.join(__dirname),
 };
 
